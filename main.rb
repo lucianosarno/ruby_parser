4e69305e0ced2aca32e0533e1e53d2603ab243a5
Luciano Sarno Soares <63410412+lucianosarno@users.noreply.github.com>
2020-10-26 07:30:19 -0300

    update parser to show only link.content instead of the whole link code

--- main.rb
+++ main.rb
@@ -2,5 +2,8 @@
 require 'Nokogiri'
 require 'open-uri'
 cagometro = Nokogiri::HTML(URI.open("http://www.cagometro.com/"))
+
 cagadas = cagometro.css("a")
-puts cagadas[5..cagadas.length - 9]
+cagadas[5..cagadas.length - 9].each do |link|
+  puts link.content
+end
