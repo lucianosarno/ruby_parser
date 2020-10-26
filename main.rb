837c64857eb88065dc6e2d90da7d2661119e24ef
Luciano Sarno Soares <63410412+lucianosarno@users.noreply.github.com>
2020-10-26 07:31:21 -0300

    rubocop offenses removed

--- main.rb
+++ main.rb
@@ -1,9 +1,8 @@
-
 require 'Nokogiri'
 require 'open-uri'
-cagometro = Nokogiri::HTML(URI.open("http://www.cagometro.com/"))
+cagometro = Nokogiri::HTML(URI.open('http://www.cagometro.com/'))
 
-cagadas = cagometro.css("a")
+cagadas = cagometro.css('a')
 cagadas[5..cagadas.length - 9].each do |link|
   puts link.content
 end
