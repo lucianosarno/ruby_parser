63d5f03c85c4f180bbfebbc11fb9780e63d4c6ac
Luciano Sarno Soares <63410412+lucianosarno@users.noreply.github.com>
2020-10-27 09:32:44 -0300

    Improve parsing algorithm / Add Gemfile and Gemfile.lock

--- bin/main.rb
+++ bin/main.rb
@@ -2,7 +2,9 @@ require 'nokogiri'
 require 'open-uri'
 cagometro = Nokogiri::HTML(URI.open('http://www.cagometro.com/'))
 
-cagadas = cagometro.css('a')
-cagadas[5..cagadas.length - 9].each do |link|
-  puts link.content
+cagadas = cagometro.xpath('//p[contains(.,"2020") or contains(.,"DE 2019")]//text() | //p[contains(.,"Dia")]//a//text() | //p[contains(.,"2020") or contains(.,"DE 2019")]//a//text()')
+# puts cagadas.content
+cagadas.each do |link|
+  puts '---'
+  puts link
 end
