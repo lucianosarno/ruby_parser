8262aa2be9182048f1b7dbc697a9a10fab99e9b1
Luciano Sarno Soares <63410412+lucianosarno@users.noreply.github.com>
2020-10-28 10:25:20 -0300

    'ask_name' method rspec work fine

--- spec/interactions_spec.rb
+++ spec/interactions_spec.rb
@@ -1,10 +1,16 @@
 require '../lib/interactions.rb'
 
 describe Interaction do
-  describe '#communicate' do
   inter = Interaction.new
+  describe '#communicate' do
     it "communicate('abc','a') should puts 'abc'" do
       expect {inter.communicate('abc','a')}.to output("abc\n").to_stdout
-    end
+    end  
+  end
+  describe '#ask_name' do
+    it "gets name 'Joe' when user inputs 'Joe'" do
+      allow_any_instance_of(Interaction).to receive(:gets).and_return('class')
+      expect(inter.ask_name).to eq('class')
+    end 
   end
 end
