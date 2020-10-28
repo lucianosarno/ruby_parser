ca6489ea2c3248d723a76c3d940757fb03981340
Luciano Sarno Soares <63410412+lucianosarno@users.noreply.github.com>
2020-10-28 10:25:55 -0300

    'ask_name' test description change

--- spec/interactions_spec.rb
+++ spec/interactions_spec.rb
@@ -8,7 +8,7 @@ describe Interaction do
     end  
   end
   describe '#ask_name' do
-    it "gets name 'Joe' when user inputs 'Joe'" do
+    it "gets the name inputed by user" do
       allow_any_instance_of(Interaction).to receive(:gets).and_return('class')
       expect(inter.ask_name).to eq('class')
     end 
